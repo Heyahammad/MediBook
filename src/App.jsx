d86e3e71d97dff0cf28962daf8743646947881bd
MD. FAISAL AHAMMAD <fahad.ahmed2456@gmail.com>
2024-12-08 09:07:19 +0600

    12-8-24-2nd

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Doctors from "./pages/Doctors";
-import Login from "./pages/Login";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import MyProfile from "./pages/MyProfile";
-import MyAppointment from "./pages/Myappointments";
-import Appointment from "./pages/Appointment";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-
-const App = () => {
+import React from 'react'
+import Navbar from '../src/components/Navbar'
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import Myprofile from './pages/Myprofile'
+import Myappointments from './pages/Myappointments'
+import Appointment from './pages/Appointment'
+import Footer from './components/Footer'
+function App() {
   return (
-    <div className="mx-4 sm:mx-[10%]">
+    <div className='mx-4 sm:mx-[10%]'>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/doctors" element={<Doctors />} />
-        <Route path="/doctors/:speciality" element={<Doctors />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/my-profile" element={<MyProfile />} />
-        <Route path="/my-appointment" element={<MyAppointment />} />
-        <Route path="/appointment/:docId" element={<Appointment />} />
+        <Route path="/contacts" element={<Contact/>} />
+        <Route path="/doctors" element={<Doctors/>} />
+        <Route path="/doctors/:speciality" element={<Doctors/>} />
+        <Route path="/Login" element={<Login/>} />
+        <Route path="/my-profile" element={<Myprofile/>} />
+        <Route path="/my-appointments" element={<Myappointments/>} />
+        <Route path="/appointments/:docId" element={<Appointment/>} />
       </Routes>
-      <Footer />
+      <Footer/>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
